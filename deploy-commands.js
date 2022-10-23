8057da3856796904b44e11987f0caf7c15c5d012
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-23 22:35:36 +0200

    fix deploy-commands script

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,13 +2,14 @@ const { REST, Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 
+const commandsPath = '.src/commands'
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+    const command = require(`${commandsPath}/${file}`);
     commands.push(command.data.toJSON());
 }
 
