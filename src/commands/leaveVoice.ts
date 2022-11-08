6bf8f52f297b10f56fc622dca1b5b704addbef67
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-08 19:31:15 +0100

    convert leaveVoice to ts

---
+++ src/commands/leaveVoice.ts
@@ -0,0 +1,32 @@
+import { Interaction, SlashCommandBuilder } from "discord.js";
+import { getVoiceConnection } from "@discordjs/voice";
+import { validateInteraction } from "../util/validateInteraction";
+
+export const data = new SlashCommandBuilder()
+  .setName("leavevoice")
+  .setDescription("Leaves from the voice channel.");
+
+export async function execute(rawInteraction: Interaction) {
+  const result = validateInteraction(rawInteraction);
+  if (result instanceof Error) {
+    console.error(result.message);
+    return;
+  }
+  const { interaction, guild } = result;
+
+  // get the connection
+  const connection = getVoiceConnection(guild.id);
+
+  // if there is no connection, return
+  if (!connection) {
+    await interaction.reply({
+      content: "I am not in a voice channel!",
+      ephemeral: true,
+    });
+    return;
+  }
+
+  // destroy the connection
+  connection.destroy();
+  await interaction.reply({ content: "Left Voice Channel!", ephemeral: true });
+}
