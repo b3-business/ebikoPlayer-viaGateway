769ec309be8855d7885c12a6d2e0758819320681
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-08 20:00:54 +0100

    remove unused code

--- src/util/isGuildMember.ts
+++ src/util/isGuildMember.ts
@@ -3,6 +3,5 @@ import { APIInteractionGuildMember, GuildMember } from "discord.js";
 export function isGuildMember(
   member: GuildMember | APIInteractionGuildMember,
 ): member is GuildMember {
-  // return Object.getOwnPropertyNames(member).includes("voice");
   return member instanceof GuildMember;
 }
