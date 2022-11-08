8d241ea2da815aa4d95857fbd1105b254e2d6c4c
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-08 19:18:11 +0100

    convert index.js to .ts

--- index.ts
+++ index.ts
@@ -1,44 +1,43 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+import fs from "node:fs";
+import path from "node:path";
+import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
+import { initAudioPlayer } from "./src/player.js";
 
-const { initAudioPlayer } = require('./src/player.js');
+const { DISCORD_TOKEN } = process.env;
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
 });
 
-const { DISCORD_TOKEN } = process.env;
+const commands = new Collection();
 
-client.commands = new Collection();
-
-const commandsPath = path.join(__dirname, 'src', 'commands');
+const commandsPath = path.join(__dirname, "src", "commands");
 const commandFiles = fs
   .readdirSync(commandsPath)
-  .filter((file) => file.endsWith('.js'));
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
   const command = require(filePath);
   // Set a new item in the Collection with the key as the command name and the value as the exported module
-  if ('data' in command && 'execute' in command) {
-    client.commands.set(command.data.name, command);
+  if ("data" in command && "execute" in command) {
+    commands.set(command.data.name, command);
   } else {
     console.log(
-      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
     );
   }
 }
 
 client.once(Events.ClientReady, () => {
   initAudioPlayer();
-  console.log('Ready!');
+  console.log("Ready!");
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  const command = interaction.client.commands.get(interaction.commandName);
+  const command = commands.get(interaction.commandName);
 
   if (!command) {
     console.error(`No command matching ${interaction.commandName} was found.`);
@@ -50,7 +49,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
   } catch (error) {
     console.error(error);
     await interaction.reply({
-      content: 'There was an error while executing this command!',
+      content: "There was an error while executing this command!",
       ephemeral: true,
     });
   }
