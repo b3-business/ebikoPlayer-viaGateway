861bfca88d218d000a02f9884519a925c724534b
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-08 20:35:36 +0100

    change anfangs und endmusik

--- src/commands/loadMusic.ts
+++ src/commands/loadMusic.ts
@@ -6,8 +6,10 @@ import { validateInteraction } from "../util/validateInteraction";
 
 const sources = {
   bennyPath: "/Users/bjesuiter/02 The Saddest Noise.m4a",
-  anfang: "/Users/bjesuiter/temp/03 Auserkoren.mp3",
-  ende: "/Users/bjesuiter/temp/11 Friede auf Erden.mp3",
+  anfang:
+    "/Users/bjesuiter/@Sync/bjesuiter-nextcloud/#Bibliotheken/Music/JB Core/Bruno Gröning Freundeskreis/Lebenstanz/04 Aufbruch zur Burg.mp3",
+  ende:
+    "/Users/bjesuiter/@Sync/bjesuiter-nextcloud/#Bibliotheken/Music/JB Core/Bruno Gröning Freundeskreis/Weit/1-03 Moment mal.m4a",
   bastiPath: "/home/ebiko/Music/Games und Filme (music)/ff8/02 - ride on.mp3",
   url:
     "https://assets.contentstack.io/v3/assets/blt731acb42bb3d1659/blt5ef06db148ba07a7/5fd95c9a752123476ba05287/TFT_Club2021_Mus_Full_MasterQC_201012.mp3",
