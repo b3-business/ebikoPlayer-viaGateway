51e4f265f2486f24aaf77e9fd8e4ffc5ebfdec22
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-08 19:25:28 +0100

    write validateInteraction function

---
+++ src/util/validateInteraction.ts
@@ -0,0 +1,28 @@
+import { Interaction } from "discord.js";
+import { isGuildMember } from "./isGuildMember";
+
+export function validateInteraction(interaction: Interaction) {
+  if (!interaction) {
+    console.error(`Tried to execute a command without an interaction`);
+    return new Error(`EXECUTION_MISSES_INTERACTION`);
+  }
+
+  if (!interaction.isChatInputCommand()) {
+    console.info(`Interaction is not a ChatInputCommand: `, interaction);
+    return new Error(`NO_CHAT_COMMAND`);
+  }
+
+  const member = interaction.member;
+  if (member === null || !isGuildMember(member)) {
+    console.error(`interaction.member is not a GuildMember`);
+    return new Error(`MEMBER_IS_NOT_A_GUILD_MEMBER`);
+  }
+
+  const guild = interaction.guild;
+  if (!guild) {
+    console.error(`Interaction has no guild attached! `, interaction);
+    return new Error(`INTERACTION_MISSES_GUILD`);
+  }
+
+  return { interaction, member, guild };
+}
