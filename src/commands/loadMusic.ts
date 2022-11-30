8dea24bd990cd823bd399bacf0644e2b07e9a78f
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-30 18:29:53 +0100

    extract music sources

--- src/commands/loadMusic.ts
+++ src/commands/loadMusic.ts
@@ -3,17 +3,7 @@ import { createAudioResource, getVoiceConnection } from "@discordjs/voice";
 import { getAudioPlayer } from "../player";
 import { Interaction } from "discord.js";
 import { validateInteraction } from "../util/validateInteraction";
-
-const sources = {
-  bennyPath: "/Users/bjesuiter/02 The Saddest Noise.m4a",
-  anfang:
-    "/Users/bjesuiter/@Sync/bjesuiter-nextcloud/#Bibliotheken/Music/JB Core/Bruno Gröning Freundeskreis/Lebenstanz/04 Aufbruch zur Burg.mp3",
-  ende:
-    "/Users/bjesuiter/@Sync/bjesuiter-nextcloud/#Bibliotheken/Music/JB Core/Bruno Gröning Freundeskreis/Weit/1-03 Moment mal.m4a",
-  bastiPath: "/home/ebiko/Music/Games und Filme (music)/ff8/02 - ride on.mp3",
-  url:
-    "https://assets.contentstack.io/v3/assets/blt731acb42bb3d1659/blt5ef06db148ba07a7/5fd95c9a752123476ba05287/TFT_Club2021_Mus_Full_MasterQC_201012.mp3",
-};
+import { sources } from "../music/sources";
 
 export const data = new SlashCommandBuilder()
   .setName("loadmusic")
---
+++ src/music/sources.ts
@@ -0,0 +1,10 @@
+export const sources = {
+  bennyPath: "/Users/bjesuiter/02 The Saddest Noise.m4a",
+  anfang:
+    "/Users/bjesuiter/@Sync/bjesuiter-nextcloud/#Bibliotheken/Music/JB Core/Bruno Gröning Freundeskreis/Lebenstanz/04 Aufbruch zur Burg.mp3",
+  ende:
+    "/Users/bjesuiter/@Sync/bjesuiter-nextcloud/#Bibliotheken/Music/JB Core/Bruno Gröning Freundeskreis/Weit/1-03 Moment mal.m4a",
+  bastiPath: "/home/ebiko/Music/Games und Filme (music)/ff8/02 - ride on.mp3",
+  url:
+    "https://assets.contentstack.io/v3/assets/blt731acb42bb3d1659/blt5ef06db148ba07a7/5fd95c9a752123476ba05287/TFT_Club2021_Mus_Full_MasterQC_201012.mp3",
+};
