f80cbdaea9650796b9c7140b1a5c5c85573e134b
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-30 18:31:59 +0100

    add new command: anfang

---
+++ src/commands/anfang.ts
@@ -0,0 +1,40 @@
+import { SlashCommandBuilder } from "discord.js";
+import { createAudioResource, getVoiceConnection } from "@discordjs/voice";
+import { getAudioPlayer } from "../player";
+import { Interaction } from "discord.js";
+import { validateInteraction } from "../util/validateInteraction";
+import { sources } from "../music/sources";
+
+export const data = new SlashCommandBuilder()
+  .setName("anfang")
+  .setDescription("Loads the music resource for Anfang.");
+
+export async function execute(rawInteraction: Interaction) {
+  const result = validateInteraction(rawInteraction);
+  if (result instanceof Error) {
+    console.error(result.message);
+    return;
+  }
+  const { interaction, guild } = result;
+
+  // get the voice connection
+  const connection = getVoiceConnection(guild.id);
+
+  // if there is no connection, return
+  if (!connection) {
+    await interaction.reply({
+      content: "I am not in a voice channel!",
+      ephemeral: true,
+    });
+    return;
+  }
+
+  const srcPath = sources["anfang"];
+  const player = getAudioPlayer();
+  const resource = createAudioResource(srcPath);
+
+  // play the player with the new resouce
+  player.play(resource);
+
+  await interaction.reply({ content: `Loaded ${srcPath}`, ephemeral: true });
+}
