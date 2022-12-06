7225636a1e2598ed7de2bd480232a8b565eb177a
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-12-06 21:04:16 +0100

    update music

--- src/music/sources.ts
+++ src/music/sources.ts
@@ -1,9 +1,9 @@
 export const sources = {
   bennyPath: "/Users/bjesuiter/02 The Saddest Noise.m4a",
   anfang:
-    "/Users/bjesuiter/@Sync/bjesuiter-nextcloud/#Bibliotheken/Music/JB Core/Bruno Gröning Freundeskreis/Lebenstanz/04 Aufbruch zur Burg.mp3",
+    "/Users/bjesuiter/@Sync/bjesuiter-nextcloud/#Bibliotheken/Music/JB Core/Bruno Gröning Freundeskreis/Liebet das Leben/02 Ich Will Mein Leben.m4a",
   ende:
-    "/Users/bjesuiter/@Sync/bjesuiter-nextcloud/#Bibliotheken/Music/JB Core/Bruno Gröning Freundeskreis/Weit/1-03 Moment mal.m4a",
+    "/Users/bjesuiter/@Sync/bjesuiter-nextcloud/#Bibliotheken/Music/JB Core/Bruno Gröning Freundeskreis/Werke Alter Meister/12 3. Satz _Allegro_.m4a",
   bastiPath: "/home/ebiko/Music/Games und Filme (music)/ff8/02 - ride on.mp3",
   url:
     "https://assets.contentstack.io/v3/assets/blt731acb42bb3d1659/blt5ef06db148ba07a7/5fd95c9a752123476ba05287/TFT_Club2021_Mus_Full_MasterQC_201012.mp3",
