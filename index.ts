f88034b38caabf0b9c354d10f808e73d016be48f
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-09-12 22:35:18 +0200

    add /health healthcheck endpoint

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { Hono } from "hono";
 import { clientPromise } from "./src/util/DiscordClient";
 const { DISCORD_TOKEN } = process.env;
 
@@ -5,6 +6,19 @@ async function main() {
   const client = await clientPromise;
   await client.login(DISCORD_TOKEN);
 }
+
+/**
+ * Adding some api routes to this discord bot server
+ */
+const app = new Hono();
+app.get("/health", (c) => {
+  return c.text("Healthcheck successful", 204);
+});
+
+export default {
+  port: 3000,
+  fetch: app.fetch,
+};
 
 /**
  * Run main function and catch top-level errors
