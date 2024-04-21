6c2a64ab9b6cf9fdff03e7f899acc8613041f609
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-04-21 16:53:50 +0200

    add file preloading

--- src/storage/minio-client.ts
+++ src/storage/minio-client.ts
@@ -1,10 +1,12 @@
-import { Client } from "minio";
+import { BucketItem, Client } from "minio";
 import { env } from "../util/env";
 
 // Uses the asnyc setup to be able to load initial data, like the folder structure etc.
 export const minioClientPromise: Promise<Client> = initMinioS3Client();
 
 export const bucketName = env.MINIO_S3_BUCKET_NAME;
+
+export const fsCache: Array<BucketItem> = [];
 
 export async function initMinioS3Client() {
   const client = new Client({
@@ -15,5 +17,16 @@ export async function initMinioS3Client() {
     secretKey: env.MINIO_S3_SECRET_KEY,
   });
 
+  // preload files in bucket
+  const objectList = client.listObjectsV2(bucketName, "", true);
+  objectList.on("data", function (obj) {
+    fsCache.push(obj);
+  });
+
   return client;
 }
+
+// export class MinioFsNode {
+//   constructor(private readonly isDir: boolean) {
+//   }
+// }
