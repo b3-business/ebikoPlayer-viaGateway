fbaa6ce1f0467a15a46123fe1fa1af0652646f6a
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-04-21 13:52:18 +0200

    add env parsing

---
+++ src/util/env.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+const EnvSchema = z.object({
+  BGF_GUILD_ID: z.string(),
+  BRUDI_GUILD_ID: z.string(),
+  CLIENT_ID: z.string(),
+  DISCORD_TOKEN: z.string(),
+  MINIO_S3_ACCESS_KEY: z.string(),
+  MINIO_S3_SECRET_KEY: z.string(),
+});
+
+export type Env = z.infer<typeof EnvSchema>;
+
+// Throws when not all required environment variables are set
+export const env: Env = EnvSchema.parse(process.env);
