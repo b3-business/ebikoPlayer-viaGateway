f0964740927ad5a3b4481ed683832dcf6a045fd2
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-04-21 16:55:34 +0200

    add example structure for file items in minio client

--- src/storage/minio-client.ts
+++ src/storage/minio-client.ts
@@ -3,9 +3,7 @@ import { env } from "../util/env";
 
 // Uses the asnyc setup to be able to load initial data, like the folder structure etc.
 export const minioClientPromise: Promise<Client> = initMinioS3Client();
-
 export const bucketName = env.MINIO_S3_BUCKET_NAME;
-
 export const fsCache: Array<BucketItem> = [];
 
 export async function initMinioS3Client() {
@@ -18,6 +16,13 @@ export async function initMinioS3Client() {
   });
 
   // preload files in bucket
+  // Example structure
+  // {
+  //   name: 'Bruno Gröning - Freundeskreis_MP3/Mitsing CD 2003_MP3/02 Der Heilstrom.mp3',
+  //   lastModified: 2024-02-06T21:32:31.767Z,
+  //   etag: '310e05efc07dc39419402a521336ccfb',
+  //   size: 8367227
+  // }
   const objectList = client.listObjectsV2(bucketName, "", true);
   objectList.on("data", function (obj) {
     fsCache.push(obj);
